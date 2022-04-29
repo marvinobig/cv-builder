64d0e8987db5d4269152e593d30718be188ce280
Marvin obig <obig.marv@gmail.com>
2022-04-29 18:21:49 +0100

    add navbar component & import router link

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <p className="nav__title">CV Builder</p>
+      </Link>
+      <Link to="/start">
+        <button className="btn btn--green">Start</button>
+      </Link>
+    </nav>
+  );
+}
+
+export default Nav;
