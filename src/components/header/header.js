02f7ada452d93f97ef1702bcbb53c51547d33314
Marvin obig <obig.marv@gmail.com>
2022-04-29 18:20:29 +0100

    add home page header component

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <p className="header__p1">
+        Welcome to the only CV Builder you'll ever need.
+      </p>
+      <p className="header__p2">
+        Whether you're applying for work experience, part-time job or your first
+        full-time job.
+      </p>
+      <p className="header__p3">You're in the right place.</p>
+      <Link to="/start">
+        <button className="btn btn--green">Start</button>
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
