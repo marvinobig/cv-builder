7471e189f139a501d6c2574dad4e807ff7b613aa
Marvin obig <obig.marv@gmail.com>
2022-05-01 12:41:28 +0100

    insert state in parent component

--- src/components/RouterSwitch.js
+++ src/components/RouterSwitch.js
@@ -1,17 +1,46 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Nav from "./navbar/navbar";
 import App from "./main/App";
 import CVForm from "./form/CVForm";
 import CV from "./CV/CV";
 
 function RouterSwitch() {
+  const [personalInfo, setPersonalInfo] = useState({});
+  const [education, setEducation] = useState([]);
+  const [work, setWork] = useState([]);
+  const [skill, setSkill] = useState([]);
+
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
         <Route index element={<App />} />
-        <Route path="/start" element={<CVForm />} />
-        <Route path="/create" element={<CV />} />
+        <Route
+          path="/start"
+          element={
+            <CVForm
+              setPersonalInfo={setPersonalInfo}
+              setEducation={setEducation}
+              setWork={setWork}
+              setSkill={setSkill}
+              education={education}
+              work={work}
+              skill={skill}
+            />
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <CV
+              personalInfo={personalInfo}
+              education={education}
+              work={work}
+              skill={skill}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
